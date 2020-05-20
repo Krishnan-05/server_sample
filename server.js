9642a5c61104474bf9f3a30d7900dec311dacca6
Krishnan-05 <krishnaa.ec21@gmail.com>
2020-05-20 17:03:35 +0530

    lab2 changes

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
 var express = require('express');
 var app = express();
-app.listen(8000,function(){
-    console.log('its works');
 
-})
-app.get('/whoiam/:name', function (req, res) {
+var body_parser = require('body-parser');
+
+app.use(body_parser.json());
+
+
+
+var file = require('fs');
+
+
+var server = app.listen(8001, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
+ 
+ app.get('/whoiam/:name', function (req, res) {
+    // First read existing users.
+
     var name = req.params.name;
         var output;
        console.log( name );
-       if (name == 'Karthik') {
+       if (name == 'jey') {
            output = name + " is NEC Alumni" ;
        } else {
          output = name + " is current student" ;
        }
        res.end( JSON.stringify(output));
     });
-    app.get('/today/:name', function (req, res) {
-        var name = req.params.name;
-            var output;
-           console.log( name );
-           if (name == 'Sunday') {
-               output = name + " is a Good day" ;
-           } else {
-             output = name + " is a better day" ;
-           }
-           res.end( JSON.stringify(output));
-        });
+    
+app.get('/student',function(req,res){
+
+  const data = file.readFileSync("student.json","utf-8");
+  const dataStr = JSON.parse(data);
+  res.end( JSON.stringify(dataStr));
+
+});
+
+app.post('/add',function(req,res){
+
+  const student  = req.body;
+  console.log('Adding new student: ', student);
+
+  res.end( JSON.stringify("students updated successfully"));
+
+});
